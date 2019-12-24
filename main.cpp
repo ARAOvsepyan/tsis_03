955182f0bc9d5182722f69b9710340ca1e0a2a44
Ara Ovsepyan <32188396+ARAOvsepyan@users.noreply.github.com>
2019-12-24 21:51:22 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,11 +2,8 @@
 
 int main() {
     std::cout << "function: cos(x)th(x)" << "\tinterval: [ 7; 11 ]\n\n";
-    secondFunction sFun;
-    function Fun;
-    double a3 = 7.0, b3 = 11.0;
-    otzhig(sFun, a3, b3);
+    otzhig(0);
     std::cout << std::endl;
-    otzhig(Fun, a3, b3);
+    otzhig(1);
     return 0;
 }
